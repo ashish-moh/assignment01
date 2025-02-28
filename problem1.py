eb482492157606a6bbd0376ed9f716cee17fdc81
ashish-moh <mohanty.ashish.96@gmail.com>
2025-02-28 17:04:43 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+print(f"Addition of {num1} + {num2}: {addition}")
+print(f"Subtraction of {num1} - {num2}: {subtraction}")
+print(f"Multiplication of {num1} * {num2}: {multiplication}")
+print(f"Division of {num1} / {num2}: {division}")
+
+
+
